48c18c89461e540477d805ec54047878daba9bbe
tsoumdoa <tsoumdoa@gmail.com>
2024-04-03 14:45:46 +0800

    as fast as standard buffered reader can get - 1mil like in 11.34 secs

---
+++ src/main.zig
@@ -0,0 +1,81 @@
+const std = @import("std");
+const Data = struct {
+    low: f16,
+    high: f16,
+    sum: f16,
+    count: f16,
+};
+
+pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
+    const stdo = std.io.getStdOut();
+    var bufout = std.io.bufferedWriter(stdo.writer());
+    _ = bufout;
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    var file = try std.fs.cwd().openFile("./data/measurements.txt", .{});
+    defer file.close();
+
+    const file_size = (try file.stat()).size;
+    try stdout.print("file size: {}\n", .{file_size});
+
+    var buffered = std.io.bufferedReader(file.reader());
+    var reader = buffered.reader();
+
+    var line = std.ArrayList(u8).init(allocator);
+    var buffered_line_writer = std.io.bufferedWriter(line.writer());
+    _ = buffered_line_writer;
+    defer line.deinit();
+
+    var line_count: usize = 0;
+
+    var map = std.StringHashMap(Data).init(allocator);
+    defer map.deinit();
+
+    while (true) : (line.clearRetainingCapacity()) {
+        reader.streamUntilDelimiter(line.writer(), '\n', null) catch |err| switch (err) {
+            error.EndOfStream => break,
+            else => return err,
+        };
+        line_count += 1;
+        // if (line_count % 10000000 == 0) {
+        // try bufout.writer().print("line: {}\n", .{line_count});
+        // }
+
+        // try bufout.writer().print("line: {s}\n", .{line.items});
+        // var line_buffer = allocator.dupe(u8, line.items) catch |err| return err;
+        // _ = line_buffer;
+
+        // var iter = std.mem.split(u8, line_buffer, ";");
+        //
+        // const name = iter.next().?;
+        // const val = try std.fmt.parseFloat(f16, iter.next().?);
+        //
+        // const entry = try map.getOrPut(name);
+        //
+        // if (entry.found_existing) {
+        //     if (val < entry.value_ptr.low) {
+        //         entry.value_ptr.low = val;
+        //     } else if (val > entry.value_ptr.high) {
+        //         entry.value_ptr.high = val;
+        //     }
+        //     entry.value_ptr.sum += val;
+        //     entry.value_ptr.count += 1;
+        // } else {
+        //     entry.value_ptr.low = val;
+        //     entry.value_ptr.high = val;
+        //     entry.value_ptr.sum = val;
+        //     entry.value_ptr.count = 1;
+        // }
+        // try bufout.writer().print("line: {}\n", .{line_count});
+        // }
+
+        // try bufout.writer().print("Hey", .{});
+        // var iterator = map.iterator();
+        // while (iterator.next()) |entry| {
+        //     try stdout.print("{s}={d:.1}/{d:.1}/{d:.1}\n", .{ entry.key_ptr.*, entry.value_ptr.low, entry.value_ptr.sum / entry.value_ptr.count, entry.value_ptr.high });
+    }
+    try stdout.print("line: {d:}\n", .{line_count});
+}
