8390df85b72ffc84c66451e0587e179a0b54821f
tsoumdoa <tsoumdoa@gmail.com>
2024-04-05 22:20:46 +0800

    abt 20 secs now...

--- src/main.zig
+++ src/main.zig
@@ -2,12 +2,13 @@ const std = @import("std");
 const Thread = std.Thread;
 const Mutex = Thread.Mutex;
 const Allocator = std.mem.Allocator;
+const Data = struct { low: f16, high: f16, sum: f16, count: i32 };
 
 const SharedData = struct {
     mutex: Mutex,
     value: i32,
     map: std.StringHashMap(Data),
-    keyArray: std.ArrayList([]u8),
+    keyArray: std.ArrayList([]const u8),
 
     pub fn incrementCount(self: *SharedData) void {
         self.mutex.lock();
@@ -15,26 +16,42 @@ const SharedData = struct {
         self.value += 1;
     }
 
-    pub fn addMap(self: *SharedData, name: []u8, val: f16) !void {
+    pub fn mergeMaps(self: *SharedData, map: std.StringHashMap(Data)) !void {
         self.mutex.lock();
         defer self.mutex.unlock();
-        const entry = try self.map.getOrPut(name);
-        if (entry.found_existing) {
-            entry.value_ptr.low = @min(val, entry.value_ptr.low);
-            entry.value_ptr.high = @max(val, entry.value_ptr.high);
-            entry.value_ptr.sum += val;
-            entry.value_ptr.count += 1;
-        } else {
-            entry.value_ptr.low = val;
-            entry.value_ptr.high = val;
-            entry.value_ptr.sum = val;
-            entry.value_ptr.count = 1;
-            try self.keyArray.append(name);
+        var it = map.iterator();
+        while (it.next()) |entry| {
+            var prev = try self.map.getOrPut(entry.key_ptr.*);
+            if (prev.found_existing) {
+                prev.value_ptr.low = @min(prev.value_ptr.low, entry.value_ptr.low);
+                prev.value_ptr.high = @max(prev.value_ptr.high, entry.value_ptr.high);
+                prev.value_ptr.sum += entry.value_ptr.sum;
+                prev.value_ptr.count += entry.value_ptr.count;
+            } else {
+                prev.value_ptr.low = entry.value_ptr.low;
+                prev.value_ptr.high = entry.value_ptr.high;
+                prev.value_ptr.sum = entry.value_ptr.sum;
+                prev.value_ptr.count = entry.value_ptr.count;
+                try self.keyArray.append(entry.key_ptr.*);
+            }
         }
     }
 };
 
-const Data = struct { low: f16, high: f16, sum: f16, count: f16 };
+fn updateMap(map: *std.StringHashMap(Data), name: []u8, val: f16) !void {
+    var entry = try map.getOrPut(name);
+    if (entry.found_existing) {
+        entry.value_ptr.low = @min(val, entry.value_ptr.low);
+        entry.value_ptr.high = @max(val, entry.value_ptr.high);
+        entry.value_ptr.sum += val;
+        entry.value_ptr.count += 1;
+    } else {
+        entry.value_ptr.low = val;
+        entry.value_ptr.high = val;
+        entry.value_ptr.sum = val;
+        entry.value_ptr.count = 1;
+    }
+}
 
 pub fn main() !void {
     if (.windows == @import("builtin").os.tag) {
@@ -62,21 +79,20 @@ pub fn main() !void {
     );
     defer std.os.munmap(ptr);
 
-    var map = std.StringHashMap(Data).init(allocator);
-    defer map.deinit();
+    var shared_map = std.StringHashMap(Data).init(allocator);
+    defer shared_map.deinit();
 
-    var key_array = std.ArrayList([]u8).init(allocator);
+    var key_array = std.ArrayList([]const u8).init(allocator);
     defer key_array.deinit();
 
     var shared_data = SharedData{
         .mutex = Mutex{},
         .value = 0,
-        .map = map,
+        .map = shared_map,
         .keyArray = key_array,
     };
     try foldRow(ptr[0..(length - 1)], &shared_data);
 
-    // try stdout.print("shared_data: {d}\n", .{shared_data.value});
     std.mem.sort([]const u8, shared_data.keyArray.items, {}, (struct {
         fn lessThan(_: void, a: []const u8, b: []const u8) bool {
             return std.mem.lessThan(u8, a, b);
@@ -85,32 +101,27 @@ pub fn main() !void {
 
     for (shared_data.keyArray.items) |name| {
         const entry = shared_data.map.get(name).?;
-        try stdout.print("{s}={d:.1}/{d:.1}/{d:.1}\n", .{ name, entry.low, entry.sum / entry.count, entry.high });
+        try stdout.print("{s}={d:.1}/{d:.1}/{d:.1}\n", .{ name, entry.low, entry.sum / @as(f16, @floatFromInt(entry.count)), entry.high });
     }
-    //
-    // var iterator = shared_data.map.iterator();
-    // while (iterator.next()) |entry| {
-    //     try stdout.print("{s}={d:.1}/{d:.1}/{d:.1}\n", .{ entry.key_ptr.*, entry.value_ptr.low, entry.value_ptr.sum / entry.value_ptr.count, entry.value_ptr.high });
-    // }
 }
 
 fn foldRow(ptr: []u8, shared_data: *SharedData) !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
-    _ = allocator;
     // const max_chunk_size = 1_024_000_000;
-    // const max_chunk_size = 512_000_000;
+    const max_chunk_size = 512_000_000;
     // const max_chunk_size = 256_000_000;
     // const max_chunk_size = 128_000_000;
-    const max_chunk_size = 64_000_000;
-    // const max_chunk_size = 32_000_000;
-    // const max_chunk_size = 16_000_000;
-    const stdout = std.io.getStdOut().writer();
-    _ = stdout;
+    // const max_chunk_size = 64_000_000;
+    // const stdout = std.io.getStdOut().writer();
+    // _ = stdout;
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    var map = std.StringHashMap(Data).init(allocator);
+    // defer map.deinit();
 
     var prev_line: usize = 0;
     var prev_delimiter: usize = 0;
-    if (ptr.len < max_chunk_size) {
+    const len = ptr.len;
+    if (len < max_chunk_size) {
         for (ptr, 0..) |c, indx| {
             if (c == ';') {
                 prev_delimiter = indx;
@@ -119,15 +130,16 @@ fn foldRow(ptr: []u8, shared_data: *SharedData) !void {
                 const name = ptr[prev_line..prev_delimiter];
                 const val = ptr[prev_delimiter + 1 .. indx];
                 const cast_val = try std.fmt.parseFloat(f16, val);
-                try shared_data.addMap(name, cast_val);
+                try updateMap(&map, name, cast_val);
                 prev_line = indx + 1;
                 // shared_data.incrementCount();
             }
         }
+        try shared_data.mergeMaps(map);
         return;
     }
 
-    var mid = ptr.len / 2;
+    var mid = len / 2;
 
     while (ptr[mid] != '\n') : (mid += 1) {}
 
